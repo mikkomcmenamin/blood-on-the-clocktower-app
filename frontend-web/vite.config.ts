a2642a5bf2074c9ba5a70f8e05de9bcbf212e829
jussisaurio <jussi.saurio@gmail.com>
2022-12-18 18:50:54 +0200

    Some more mobile adjustments

--- frontend-web/vite.config.ts
+++ frontend-web/vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.webm'],
-})
+  assetsInclude: ["**/*.webm"],
+  server: {
+    // listen on all interfaces so that the app can be accessed eg via a phone on the same network
+    host: "0.0.0.0",
+  },
+});
