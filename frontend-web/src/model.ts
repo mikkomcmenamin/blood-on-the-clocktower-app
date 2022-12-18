f0735e788ae2f5c73c16dac8c3a97b937317ed47
jussisaurio <jussi.saurio@gmail.com>
2022-12-18 19:48:00 +0200

    Sketch out some game state ideas

--- frontend-web/src/model.ts
+++ frontend-web/src/model.ts
@@ -1,9 +1,29 @@
 import { nextId } from "./util";
 
+type Team = "good" | "evil";
+
+type Phase =
+  | { phase: "day"; nomination: Nomination; dayNumber: number }
+  | {
+      phase: "night";
+      nightDeaths: number[] /* player IDs */;
+      nightNumber: number;
+    };
+
+type Character = string; // todo: make an union of all the BOTC characters
+
 export type Player = {
   name: string;
   id: number;
 };
+
+export type SetupStagePlayer = Player;
+
+type AliveState = { alive: true } | { alive: false; ghostVote: boolean };
+
+type CharacterState = { character: Character; team: Team };
+
+export type ActiveStagePlayer = Player & CharacterState & AliveState;
 
 export type Nomination =
   | {
@@ -19,6 +39,29 @@ export type Nomination =
       state: "inactive";
     };
 
+export type Game =
+  | {
+      stage: "setup";
+      players: SetupStagePlayer[];
+    }
+  | {
+      stage: "active";
+      players: ActiveStagePlayer[];
+      phase: Phase;
+    }
+  | {
+      stage: "finished";
+      players: ActiveStagePlayer[];
+      winningTeam: Team;
+    };
+
+export type GameInPhase<P extends Phase["phase"]> = Extract<
+  Game,
+  { stage: "active" }
+> extends infer G
+  ? G & { phase: Extract<Phase, { phase: P }> }
+  : never;
+
 export const createPlayer = (name: string): Player => ({
   name,
   id: nextId(),
@@ -29,3 +72,79 @@ export const isNominator = (player: Player, nomination: Nomination) =>
 
 export const isNominee = (player: Player, nomination: Nomination) =>
   nomination.state === "active" && nomination.nominee.id === player.id;
+
+export const transitionFromDayToNight = (
+  game: GameInPhase<"day">
+): GameInPhase<"night"> => {
+  return {
+    ...game,
+    phase: {
+      phase: "night",
+      nightDeaths: [],
+      nightNumber: game.phase.dayNumber + 1,
+    },
+  };
+};
+
+export const transitionFromNightToDay = (
+  game: GameInPhase<"night">
+): GameInPhase<"day"> => {
+  return {
+    ...game,
+    phase: {
+      phase: "day",
+      nomination: { state: "inactive" },
+      dayNumber: game.phase.nightNumber + 1,
+    },
+    players: game.players.map((player) => {
+      if (!player.alive) {
+        return player;
+      }
+
+      const aliveAndGhostVote = game.phase.nightDeaths.includes(player.id)
+        ? {
+            alive: false as const,
+            ghostVote: true,
+          }
+        : {
+            alive: true as const,
+          };
+
+      return {
+        ...player,
+        ...aliveAndGhostVote,
+      };
+    }),
+  };
+};
+
+export const transitionFromSetupToActive = (
+  game: Extract<Game, { stage: "setup" }>
+): GameInPhase<"night"> => {
+  return {
+    ...game,
+    players: game.players.map((player) => ({
+      ...player,
+      alive: true,
+      character: "Clockmaker", // TODO
+      team: "good", // TODO
+    })),
+    stage: "active",
+    phase: {
+      phase: "night",
+      nightDeaths: [],
+      nightNumber: 1,
+    },
+  };
+};
+
+export const transitionFromActiveToFinished = (
+  game: GameInPhase<"day">,
+  winningTeam: Team
+): Extract<Game, { stage: "finished" }> => {
+  return {
+    ...game,
+    stage: "finished",
+    winningTeam,
+  };
+};
