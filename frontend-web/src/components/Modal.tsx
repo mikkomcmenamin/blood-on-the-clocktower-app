7d234d66d9b6d8dc8baf37fbaae0e72c83e9f1ab
Mikko McMenamin <mikko.mcmenamin@gmail.com>
2022-12-18 23:31:21 +0200

    add fleet config, reformat Modal.tsx

--- frontend-web/src/components/Modal.tsx
+++ frontend-web/src/components/Modal.tsx
@@ -2,49 +2,49 @@ import {useRef, useState, useEffect} from 'react';
 import './Modal.scss'
 
 type ModalProps = {
-    isOpen: boolean;
-    addPlayer: (name: string) => void;
-    modalRef: React.RefObject<HTMLDivElement>;
+  isOpen: boolean;
+  addPlayer: (name: string) => void;
+  modalRef: React.RefObject<HTMLDivElement>;
 };
 
 const Modal: React.FC<ModalProps> = ({isOpen, addPlayer, modalRef}) => {
-    const playerInputRef = useRef<HTMLInputElement>(null);
-    const [newPlayerName, setNewPlayerName] = useState('');
+  const playerInputRef = useRef<HTMLInputElement>(null);
+  const [newPlayerName, setNewPlayerName] = useState('');
 
-    useEffect(() => {
-        if (isOpen) playerInputRef.current?.focus();
-    }, [isOpen]);
+  useEffect(() => {
+    if (isOpen) playerInputRef.current?.focus();
+  }, [isOpen]);
 
-    const handleNewPlayerNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setNewPlayerName(e.target.value);
-    }
+  const handleNewPlayerNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewPlayerName(e.target.value);
+  }
 
-    const handleAddPlayer = (e: any) => {
-        e.preventDefault();
-        addPlayer(newPlayerName);
-        setNewPlayerName("");
-    }
+  const handleAddPlayer = (e: any) => {
+    e.preventDefault();
+    addPlayer(newPlayerName);
+    setNewPlayerName("");
+  }
 
-    return (
-        <div className="modal">
-            <div ref={modalRef} className="modal-content">
-                <h2>Add a new player</h2>
-                <form id="add-player-form" onSubmit={handleAddPlayer}>
-                    <input
-                        ref={playerInputRef}
-                        type="text"
-                        value={newPlayerName}
-                        onChange={handleNewPlayerNameChange}
-                    />
-                    <div className="buttons">
-                        <button type="submit" onClick={handleAddPlayer}>
-                            Add
-                        </button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    );
+  return (
+    <div className="modal">
+      <div ref={modalRef} className="modal-content">
+        <h2>Add a new player</h2>
+        <form id="add-player-form" onSubmit={handleAddPlayer}>
+          <input
+            ref={playerInputRef}
+            type="text"
+            value={newPlayerName}
+            onChange={handleNewPlayerNameChange}
+          />
+          <div className="buttons">
+            <button type="submit" onClick={handleAddPlayer}>
+              Add
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
