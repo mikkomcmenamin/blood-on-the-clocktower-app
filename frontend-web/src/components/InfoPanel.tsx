bb1c116154dc274db0c3649b77b34a7d419fd62a
Mikko McMenamin <mikko.mcmenamin@gmail.com>
2022-12-21 14:38:12 +0200

    Add stylized InfoPanel

--- frontend-web/src/components/InfoPanel.tsx
+++ frontend-web/src/components/InfoPanel.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { useWindowInnerWidth } from '../hooks';
 import styled from "styled-components";
+import InfoPanelImage from "../assets/T_InfoPanel.png";
 
 const Panel = styled.div`
+  background-image: url(${InfoPanelImage});
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+  min-width: 300px;
   position: fixed;
+  margin: 2rem;
   top: 0;
   right: 0;
   padding: 1rem;
-  background: rgba(0, 0, 0, 0.6);
+  text-align: center;
+  padding: 20px 20px 30px 20px;
 `;
 
 type Props = {
