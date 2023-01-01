f102a7bd8f6ba86cd00eb2354d8dd0804f284c38
jussisaurio <jussi.saurio@gmail.com>
2023-01-01 15:00:08 +0200

    Use ChatGPT's functional programming version for 'getTwoClosestPlayers'

--- frontend-web/src/domHelpers.ts
+++ frontend-web/src/domHelpers.ts
@@ -1,24 +1,28 @@
 function getDistance(x1: number, y1: number, x2: number, y2: number) {
   return Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
 }
-// When a Player is dropped onto the .gameBoard, calculate what are the two nearest players.
-// The players can be retrieved by querying for the data-playerid attribute.
+
 type Coords = {
   x: number;
   y: number;
 };
 
+type Player = {
+  id: number;
+  element: HTMLElement;
+};
+
 export function getTwoClosestPlayers(
   playerElements: NodeListOf<Element>,
   coords: Coords
-) {
+): { closestPlayerId: number; secondClosestPlayerId: number } {
   const playerElementsArray = Array.from(playerElements).filter(
     (el): el is HTMLElement => el instanceof HTMLElement
   );
-  const playerElementsWithId = playerElementsArray.map((element) => {
+  const players: Player[] = playerElementsArray.map((element) => {
     const id = element.getAttribute("data-playerid");
     if (!id) {
-      throw Error("Player element has no data-playerid attribute");
+      throw new Error("Player element has no data-playerid attribute");
     }
     return {
       id: parseInt(id),
@@ -26,33 +30,29 @@ export function getTwoClosestPlayers(
     };
   });
 
-  let closestPlayerId = null;
-  let closestDistance = Infinity;
-  let secondClosestPlayerId = null;
-  let secondClosestDistance = Infinity;
-  for (const playerElement of playerElementsWithId) {
-    const { left, top, width, height } =
-      playerElement.element.getBoundingClientRect();
-    const centerX = left + width / 2;
-    const centerY = top + height / 2;
-    const distance = getDistance(coords.x, coords.y, centerX, centerY);
-    if (distance < closestDistance) {
-      secondClosestPlayerId = closestPlayerId;
-      secondClosestDistance = closestDistance;
-      closestPlayerId = playerElement.id;
-      closestDistance = distance;
-    } else if (distance < secondClosestDistance) {
-      secondClosestPlayerId = playerElement.id;
-      secondClosestDistance = distance;
-    }
-  }
+  const getCenter = (player: Player): Coords => {
+    const { left, top, width, height } = player.element.getBoundingClientRect();
+    return {
+      x: left + width / 2,
+      y: top + height / 2,
+    };
+  };
 
-  if (closestPlayerId == null || secondClosestPlayerId == null) {
-    throw Error("Could not find two closest players");
+  const getDistanceToCoords = (player: Player): number => {
+    const center = getCenter(player);
+    return getDistance(coords.x, coords.y, center.x, center.y);
+  };
+
+  const playersSortedByDistance = players.sort((player1, player2) => {
+    return getDistanceToCoords(player1) - getDistanceToCoords(player2);
+  });
+
+  if (playersSortedByDistance.length < 2) {
+    throw new Error("Could not find two closest players");
   }
 
   return {
-    closestPlayerId,
-    secondClosestPlayerId,
+    closestPlayerId: playersSortedByDistance[0].id,
+    secondClosestPlayerId: playersSortedByDistance[1].id,
   };
 }
